907716dd1e8751b97e1ffb1d191be5251a1978c0
GioOli7 <gio07live@gmail.com>
2021-06-08 16:30:58 +0200

    header style

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,3 +1,9 @@
-<header>
-    header here
+<header class="container">
+    <img class='logo' src=" {{ asset('images/dc-logo.png') }}" alt="logo">
+    <nav>
+        <ul>
+            <li class='active'><a href="#">COMICS</a></li>
+            <li><a href="#">NEWS</a></li>
+        </ul>
+    </nav>
 </header>
