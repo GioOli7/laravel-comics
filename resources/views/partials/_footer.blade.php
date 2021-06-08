2cf8117adb10f07e0015fdf7bea3b5dff46b7759
GioOli7 <gio07live@gmail.com>
2021-06-08 17:27:37 +0200

    footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,3 +1,15 @@
 <footer>
-    footer here
+    <div class="container">
+        <div class="content">
+            <a href="#" class="sign-up">SIGN UP NOW</a>
+            <div class="socials">
+                <h3>FOLLOW US</h3>
+                <a href="#"><img src=" {{ asset('images/footer-facebook.png') }} " alt=""></a>
+                <a href="#"><img src=" {{ asset('images/footer-periscope.png') }} " alt=""></a>
+                <a href="#"><img src=" {{ asset('images/footer-pinterest.png') }} " alt=""></a>
+                <a href="#"><img src=" {{ asset('images/footer-twitter.png') }} " alt=""></a>
+                <a href="#"><img src=" {{ asset('images/footer-youtube.png') }} " alt=""></a>
+            </div>
+        </div>
+    </div>
 </footer>
